0f63312b0989f8a257254aaeba88bb2580264af4
Eddie Moya <eddie.moya@gmail.com>
2013-10-14 16:04:12 -0500

    removing the function from this file and chaning it to an anonymous function attached directly to a hook

--- plugin.php
+++ plugin.php
@@ -7,6 +7,9 @@ Author: Eddie Moya
 Author URI: http://eddiemoya.com
 */
 
+
+
+
 define('WPNODE_PATH', 		plugin_dir_path(__FILE__));
 
 
@@ -14,9 +17,4 @@ include (WPNODE_PATH 		. 'WP_Node_Factory.php');
 include (WPNODE_PATH 		. 'WP_Node.php');
 include (WPNODE_PATH 		. 'meta-importer.php');
 
-add_action('init', 'create_nodes', 10);
-
-function create_nodes(){
-	$node = new WP_Node_Controller('skcategory');
-}
-
+add_action('init', create_function('', 'new WP_Node_Factory("skcategory");'), 10);
